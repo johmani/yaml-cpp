f934fa8c35970084c592262ea341138de0e39672
Mohamd Johmani <mohamdaljohmani@gmail.com>
2025-02-19 15:25:21 +0300

    add premake

---
+++ premake5.lua
@@ -0,0 +1,46 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+	cppdialect  "C++latest"
+
+	targetdir (libOutputDir)
+    objdir (IntermediatesOutputDir)
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		"include/**.h"
+	}
+
+	defines
+	{
+		"YAML_CPP_STATIC_DEFINE"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "Off"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		staticruntime "Off"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "Speed"
+        symbols "Off"
